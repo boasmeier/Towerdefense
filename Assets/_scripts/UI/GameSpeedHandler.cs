1e4de8da332d9c2ac398bf6980c2b9f701cd5a98
boasmeier <62314872+boasmeier@users.noreply.github.com>
2020-11-02 21:13:42 +0100

    new, added slider for game speed

---
+++ Assets/_scripts/UI/GameSpeedHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameSpeedHandler : MonoBehaviour
+{
+    [SerializeField]
+    private Slider gameSpeedSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameSpeedSlider.onValueChanged.AddListener(delegate {ChangeGameSpeed();});   
+    }
+
+    private void ChangeGameSpeed() {
+        int value =  (int) gameSpeedSlider.value;
+        
+        switch(value) {
+            case 1: 
+                Time.timeScale = 1;
+                break;
+
+            case 2:
+                Time.timeScale = 2;
+                break;
+
+            case 3: 
+                Time.timeScale = 4;
+                break;
+
+            case 4:
+                Time.timeScale = 8;
+                break;
+        }
+        Debug.Log("Game speed set to: " + Time.timeScale + "x"); 
+    }
+}
