e2566a908a406d0ac5aa2770a753cce797466d42
boasmeier <62314872+boasmeier@users.noreply.github.com>
2020-11-19 21:21:03 +0100

    new, added UI Menue

---
+++ Assets/_scripts/UI/UIMenue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMenue : MonoBehaviour
+{
+
+    [SerializeField]
+    private Button resumeButton;
+    [SerializeField]
+    private Button restartButton;
+    [SerializeField]
+    private Button exitButton;
+
+    public event Action HandleRestart = delegate { };
+    public event Action HandleExit = delegate { };
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
