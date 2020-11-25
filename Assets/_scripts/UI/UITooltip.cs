e0ee9904dbefb6be6b2ed824d468a0bcbfc2bc9c
Tobias Heller <tobias.heller@stud.hslu.ch>
2020-11-25 11:45:21 +0100

    ui: new, add tooltips

---
+++ Assets/_scripts/UI/UITooltip.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITooltip : MonoBehaviour
+{
+    [SerializeField] Text tipText;
+    [SerializeField] List<string> tips = new List<string>();
+
+    private int currentTip = 0;
+
+    //Show a new tip every 10 seconds
+    void Start()
+    {
+        InvokeRepeating("ShowTip", 0.0f, 10f);
+    }
+        
+    //Shows the next tip
+    void ShowTip()
+    {
+        tipText.text = tips[currentTip];
+        currentTip++;
+        if (currentTip >= tips.Count)
+        {
+            currentTip = 0;
+        }
+    }
+}
