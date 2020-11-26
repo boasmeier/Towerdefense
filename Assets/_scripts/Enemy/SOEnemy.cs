75185f705020ce9caa8c6ed531cd6299c928a113
Tobias Heller <tobias.heller@stud.hslu.ch>
2020-11-26 21:29:06 +0100

    sounds: new, add enemy die sounds and global sound -> Lot to improve

--- Assets/_scripts/Enemy/SOEnemy.cs
+++ Assets/_scripts/Enemy/SOEnemy.cs
@@ -8,8 +8,10 @@ public class SOEnemy : ScriptableObject
     [SerializeField] private int health;
     [SerializeField] private int value;
     [SerializeField] private int speed;
+    [SerializeField] private AudioClip deathAudio;
 
     public int Health { get { return health; }}
     public int Speed { get { return speed; }}
     public int Value { get { return value; } }
+    public AudioClip DeathAudio { get { return deathAudio; } }
 }
