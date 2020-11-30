79070e876d3cfe726d28037f2bd7c7b1036dda1f
Silvan Wenk <silvan.wenk@stud.hslu.ch>
2020-11-30 18:58:29 +0100

    Add Enemy Death animatino

--- Assets/_scripts/Enemy/EnemyDeathController.cs
+++ Assets/_scripts/Enemy/EnemyDeathController.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyDeathController : MonoBehaviour
 {
     [SerializeField] private SOEnemy enemy;
+    [SerializeField] private float shrinkTimeSec;
+    private Animation animation;
 
     private IHealthController hc;
 
@@ -13,18 +14,28 @@ public class EnemyDeathController : MonoBehaviour
 
     private void Awake()
     {
+        animation = GetComponent<Animation>();
+        animation.Stop();
         hc = GetComponent<IHealthController>();
         hc.HandleDeath += Die;
     }
 
-    private void OnDisable() {
+    private void OnDisable()
+    {
         hc.HandleDeath -= Die;
     }
-
 
     private void Die()
     {
         HandleEnemyDeath(enemy.Value);
+        StartCoroutine(Shrink());
+    }
+
+    private IEnumerator Shrink()
+    {
+        gameObject.layer = LayerMask.NameToLayer("Default");
+        animation.Play();
+        yield return new WaitForSeconds(shrinkTimeSec);
         Destroy(gameObject);
     }
 }
