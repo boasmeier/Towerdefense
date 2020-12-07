ea7143342d17d342484a30085aee1b08ab9513ef
Tobias Heller <tobias.heller@stud.hslu.ch>
2020-12-07 19:19:12 +0100

    gameplay: new, add enemy boss, add additional waves, rebalance

--- Assets/_scripts/Shot/Shot.cs
+++ Assets/_scripts/Shot/Shot.cs
@@ -5,13 +5,20 @@ public class Shot : MonoBehaviour
     [SerializeField] private SOShot _shot;
     public int Damage { get { return _shot.Damage; } }
 
+    private LayerMask enemyLayer;
+
     private void Start()
     {
-        //TODO playSound!!!
+        enemyLayer = LayerMask.NameToLayer("Shot");
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        //TODO: Handle collision
+        /*if (collision.gameObject.layer != enemyLayer.value)
+        {
+            Destroy(gameObject);
+        }*/
+
     }
 }
